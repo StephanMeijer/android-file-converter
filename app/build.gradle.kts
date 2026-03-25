5994d9b6cf2ae7087e4a88375d907063b6179da5
Stephan Meijer <me@stephanmeijer.com>
2026-03-25 20:39:11 +0100

    feat: enable R8 minification and resource shrinking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,8 @@ android {
             if (keystoreBase64 != null) {
                 signingConfig = signingConfigs.getByName("release")
             }
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
